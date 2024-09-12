8a8c6a6d10f1ff27e1b7bb72c6fa19dfcc18fec2
Temistoclis <temistoclisxp58@gmail.com>
2024-09-12 01:16:50 -0300

    atualizando body da home

--- src/components/Listagem/index.tsx
+++ src/components/Listagem/index.tsx
@@ -1,5 +1,8 @@
 import { Botao, Descricao, Imagemrestaurante, Nota, Paragrafo, Restaurante } from "./style";
 
+
+
+
 export type Props = {
     size?: 'small' | 'big';
     children?: React.ReactNode; 
@@ -12,17 +15,24 @@ const Fragmento = ({ size = 'small', children }: Props) => (
 const Listagem = () => (
     <Restaurante>
     <Imagemrestaurante >
-        <Botao>Destaque da semana</Botao> 
-        <Botao>Japonesa</Botao>
+        <div className="botoes_imagem_restaurante">
+            <Botao>Destaque da semana</Botao> 
+            <Botao>Japonesa</Botao>
+        </div>
     </Imagemrestaurante>
     <Descricao>
-        <Fragmento size="big">Hioki Sushi
+        <div className="restaurante_nota">
+            <Fragmento size="big">Hioki Sushi </Fragmento>
             <Nota><Fragmento size="big">4.9</Fragmento><img src="public\estrela.png" alt="" /></Nota>
-        </Fragmento>
+        </div>
         <Fragmento>
-        Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!
+            <p>
+            Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos,
+            sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e 
+            qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!
+            </p>
         </Fragmento>
-        <Botao>Saiba mais</Botao> 
+       <Botao>Saiba mais</Botao> 
     </Descricao>
     </Restaurante>
 );
--- src/components/Listagem/style.ts
+++ src/components/Listagem/style.ts
@@ -5,11 +5,11 @@ import { Props } from ".";
 
 export const Restaurante = styled.div`
     border: 1px solid ${Cores.VermelhoCoral}; 
-    height: 394px;
+    height: 398px;
     width: 472px;
 `;
 
-export const Botao = styled.div<Props>`
+export const Botao = styled.button<Props>`
     background-color: ${Cores.VermelhoCoral};
     color: ${Cores.CremePessego};
     padding-top: 8px;
@@ -18,29 +18,58 @@ export const Botao = styled.div<Props>`
     padding-right: 6px;
     font-size: ${(props) => (props.size === 'big' ? '14px' : '12px')};
     font-weight: 700;
+    border:none;
+    margin:5px;
+    
 `;
 
 export const Imagemrestaurante = styled.div`
+    
     height: 217px;
     width: 472px;
     background-image:url('public/sushi.png');
-     background-size: cover;
+    background-size: cover;
+    
+    .botoes_imagem_restaurante{
+        padding:16px;
+        display:flex;
+        justify-content:flex-end;
+    }
 `;
 
 export const Descricao = styled.p`
     background-color: ${Cores.Branco};
+
+    .restaurante_nota{
+        display:flex;
+        justify-content:space-between;
+        margin:8px;
+        
+        
+        
+    }
 `;
 
 export const Paragrafo = styled.p<Props>`
     color: ${Cores.VermelhoCoral};
     font-size: ${(props) => (props.size === 'big' ? '18px' : '14px')};
     font-weight: ${(props) => (props.size === 'big' ? '700' : '400')};
+   
+    p{
+        margin-top:16px;
+        margin-left:8px;
+        margin-right:8px;
+        margin-bottom:20px;
+        
+    }
+
 `;
 
 
 export const Nota = styled.p`
 
     display:flex;
+    gap:8px;
     img {
         height: 20px;
         width: 21px;
